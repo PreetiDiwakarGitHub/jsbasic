8b981b294fe28aefe46bb861643173fff3aaf932
PreetiDiwakarGitHub <p208016@gmail.com>
2024-04-07 21:17:07 +0530

    java script

---
+++ index.js
@@ -0,0 +1,118 @@
+//  function
+
+// function start(){
+//     console.log('hello world');
+// }
+// start()
+
+// function start(){
+//     console.log('hello world');    xxxxxxxxxx
+// }
+// let print=start();
+// console.log(start());
+
+
+// function start(){
+//     let name="preeti";
+//     console.log("hello"+name);
+// }
+// start();
+
+// function start(){
+//     let fname="preeti";
+//     console.log(fname);      
+// }
+// start();
+// console.log(fname);
+
+ 
+// let fname="preeti";
+// function start(){
+//          console.log(fname);
+//      }
+//     start();
+//      console.log(fname);
+
+
+
+// let fname="tom";
+// function start(){
+//     console.log("hello"+fname);
+// }
+// start();
+
+
+// let fname="Tom";
+// function start(){
+//     console.log("hello"+fname);
+// }
+// start();
+// console.log(fname);
+
+// function start(){
+//     console.log("hello");
+// }
+// start();
+// console.log("Tom");
+// console.log("Tom");
+// console.log("Tom");
+// console.log("Tom");
+// console.log("Tom");
+// start();
+
+// let fname="john";
+// function start(){
+//     let lname="Tom";
+//     console.log("hello" + fname + "and" + lname);
+// }
+// start();
+// console.log(fname);
+
+
+// let fname="tom";
+// function start(){
+//     fname="john";
+//     console.log("hello"+fname);
+// }
+// console.log(fname);
+// console.log(fname);
+// start();
+// console.log(fname);
+
+// function fun(fname){
+//     console.log(fname);
+// }
+// fun("alka")
+
+
+// function fun(fname){
+//     console.log(fname);
+// }
+// fun("alka");
+// fun("12");
+// fun("preeti")
+
+
+// function start(city){
+//     console.log("I belongs to" + city);
+// }
+// start();
+
+// function start(city){
+//     console.log("I belongs to" +  "" +city);
+// }
+// start('pune');
+
+// function start(city,country){
+//     console.log("I belongs to " + city + "in " + country);
+// }
+// start('pune','India')
+
+
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/PreetiDiwakarGitHub/basic-js.git
+// git push -u origin main
+
