74e5716c2b99f3808d2796851c1e0a864674a358
PreetiDiwakarGitHub <p208016@gmail.com>
2024-04-10 09:20:27 +0530

    questions

---
+++ class2.js
@@ -0,0 +1,54 @@
+// function name(){
+//     console.log("hell world");
+// }
+// name();
+
+
+// function name(){
+//     console.log("Hello world");
+// }
+// name();
+// name();
+// name();
+// name();
+
+
+// let number=10;
+// function name(num){
+//     console.log(num);
+// }
+// name(number)
+
+
+// function number(num=5){
+//     console.log(num);
+// }
+// number();
+
+// function number(num=5){
+//     console.log(num);
+// }
+// number(100);
+
+// function number(num=10,name){
+//     console.log(num,name);
+// }
+// number("nucle",100);
+
+// function number(num=10,name){
+//     console.log(num,name);
+// }
+// number(100,"preeti");
+
+
+// function array(arr){
+//     console.log(arr);
+// }
+// array([10,20,30,40]);
+
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/PreetiDiwakarGitHub/Javascript.git
+// git push -u origin main
